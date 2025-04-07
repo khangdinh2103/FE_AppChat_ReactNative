c66ead6eadcbf5cf7870f3204a50f214c867b63c
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-04-07 13:02:44 +0700

    fix home page

--- fe_appchat_reactnative/src/screens/bottomtab/Home.js
+++ fe_appchat_reactnative/src/screens/bottomtab/Home.js
@@ -89,7 +89,8 @@ const Home = () => {
             placeholderTextColor="#fff"
             style={styles.searchInput}
           />
-          <Ionicons name="options-outline" size={20} color="#fff" />
+          <Ionicons name="qr-code-outline"  size={20} color="#fff" />
+          <Ionicons name="add-outline"  size={20} color="#fff" marginLeft={7}/>
         </View>
         <View style={styles.tabs}>
           <TouchableOpacity>
@@ -98,7 +99,7 @@ const Home = () => {
           <TouchableOpacity>
             <Text style={styles.inactiveTab}>Khác</Text>
           </TouchableOpacity>
-          <Ionicons name="filter-outline" size={20} color="#8E8E93" />
+          <Ionicons name="swap-vertical-outline" size={20} color="#8E8E93" />
         </View>
         <FlatList
           data={messages}
@@ -154,10 +155,10 @@ const styles = StyleSheet.create({
     borderBottomColor: "#E0E0E0",
   },
   avatar: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
+    width: 45,
+    height: 45,
+    borderRadius: 15,
+    marginRight: 20,
   },
   avatarPlaceholder: {
     backgroundColor: "#4E7DFF",
