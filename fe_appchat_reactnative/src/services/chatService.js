eee37d9d498528545d63303a0c89d28bb6a69306
QuocHuyGIT103 <quochuyab2003@gmail.com>
2025-04-13 23:06:26 +0700

    update UI chat detail with send image and done feature send image

--- fe_appchat_reactnative/src/services/chatService.js
+++ fe_appchat_reactnative/src/services/chatService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import { API_URL } from '@env';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-const API_URL = "https://1814-2a09-bac5-d46c-25d7-00-3c5-3e.ngrok-free.app";
+const API_URL = "http://192.168.2.74:5000";
 // Create axios instance with base configuration
 const chatApi = axios.create({
   baseURL: API_URL
