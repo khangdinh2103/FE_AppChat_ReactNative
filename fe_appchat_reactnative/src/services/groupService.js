702bdd5fdfda7007f4fad8f55ffedba107748b2d
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-04-21 14:30:22 +0700

    nhanh_cua_huy

--- fe_appchat_reactnative/src/services/groupService.js
+++ fe_appchat_reactnative/src/services/groupService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = "http://192.168.2.72:5000/api/group";
+const API_URL = "http://192.168.2.213:5000/api/group";
 const groupApi = axios.create({
   baseURL: API_URL,
 });
